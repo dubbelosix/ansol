5068df34f0c5ca5b268215f9cc7b9c9f54acdc36
dubbelosix <dub@06.com>
2022-10-15 06:00:26 +0000

    snapshot finder init

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-python3 /mnt/snapcheck.py
+if [ $# -eq 0 ]
+  then
+    python3 /mnt/snapcheck.py
+fi
 sudo systemctl stop sol.service
 sudo pkill -9 -f solana-sys-tuner
 sudo $(command -v solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
