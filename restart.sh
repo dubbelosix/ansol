5f0aaf24e76cb3f7dfbf4a0bb80288cb9a0385d5
dubbelosix <dub@006.com>
2022-10-16 15:56:29 +0530

    fix sys-tuner path for sudo

--- restart.sh
+++ restart.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]
 fi
 sudo systemctl stop sol.service
 sudo pkill -9 -f solana-sys-tuner
-sudo $(command -v solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
+sudo $(command -v /mnt/solana/target/release/solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
 sudo systemctl start sol.service
