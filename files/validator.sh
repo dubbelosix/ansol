f41ee10c7b6523e6107860cc48acddf66906089a
dubbelosix <dub@006.com>
2023-01-16 02:00:38 +0530

    using a different validator.sh

--- files/validator.sh
+++ files/validator.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 export SOLANA_METRICS_CONFIG="host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password"
-exec /mnt/solana/target/release/solana-validator --identity /home/solana/rpc_node.json \
---full-rpc-api \
---rpc-port 8899 \
+exec /mnt/solana/target/release/solana-validator \
+--identity /home/solana/rpc_node.json \
 --entrypoint entrypoint.mainnet-beta.solana.com:8001 \
---limit-ledger-size \
+--entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
+--rpc-port 8899 \
+--dynamic-port-range 8002-8099 \
+--no-port-check \
+--gossip-port 8001 \
+--no-untrusted-rpc \
+--no-voting \
+--private-rpc \
+--rpc-bind-address 0.0.0.0 \
+--enable-cpi-and-log-storage \
+--account-index program-id \
+--enable-rpc-transaction-history \
+--no-duplicate-instance-check \
+--wal-recovery-mode skip_any_corrupted_record \
 --log /mnt/logs/solana-validator.log \
 --accounts /mnt/solana-accounts \
 --ledger /mnt/solana-ledger \
 --snapshots /mnt/solana-snapshots \
 --no-snapshot-fetch \
---known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
---known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
---known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
---known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S
+--limit-ledger-size 400000000 \
+--rpc-send-default-max-retries 3 \
+--rpc-send-service-max-retries 3 \
+--rpc-send-retry-ms 2000 \
+--full-rpc-api \
+--accounts-index-memory-limit-mb 350 \
+--account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
+--tpu-use-quic \
+--known-validator PUmpKiNnSVAZ3w4KaFX6jKSjXUNHFShGkXbERo54xjb \
+--known-validator Ninja1spj6n9t5hVYgF3PdnYz2PLnkt7rvaw3firmjs \
+--known-validator CXPeim1wQMkcTvEHx9QdhgKREYYJD8bnaCCqPRwJ1to1 \
+--known-validator A4hyMd3FyvUJSRafDUSwtLLaQcxRP4r1BRC9w2AJ1to2 \
+--known-validator 23U4mgK9DMCxsv2StC4y2qAptP25Xv5b2cybKCeJ1to3 \
+--known-validator Ei8VLKR3chZAhJzWwj8PopeuedpQiths2ovVCQ2BCvK7 \
+--known-validator DiGifdKABxzru2KsjN3YkZZmWP9mVMYK8HWadjtPtJit
